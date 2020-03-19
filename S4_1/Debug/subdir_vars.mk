b7317e5309d19221882e1b5f8be2e4b0e24ed0ac
Bleincos <62374766+Bleincos@users.noreply.github.com>
2020-03-19 09:10:49 +0100

    Add files via upload

---
+++ S4_1/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2231.cmd 
+
+C_SRCS += \
+../ServoMoteur.c \
+../USI_msp2231.c \
+../main.c 
+
+C_DEPS += \
+./ServoMoteur.d \
+./USI_msp2231.d \
+./main.d 
+
+OBJS += \
+./ServoMoteur.obj \
+./USI_msp2231.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"ServoMoteur.obj" \
+"USI_msp2231.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"ServoMoteur.d" \
+"USI_msp2231.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../ServoMoteur.c" \
+"../USI_msp2231.c" \
+"../main.c" 
+
+
