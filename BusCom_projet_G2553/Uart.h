c0e2b69bc4a7d021b4d37c66d8016d1715ee8da9
MaGit98 <marwane.ouldahmed1@gmail.com>
2020-04-02 09:44:54 +0200

    màj moteur robot

--- BusCom_projet_G2553/Uart.h
+++ BusCom_projet_G2553/Uart.h
@@ -12,10 +12,17 @@
 
 
 void init_UART( void );
-
 void envoi_msg_UART(unsigned char * );
 void interpreteur( void );
 void interpreteur_robot( void );
+void go (void);
+void stop (void);
+void forward (void);
+void back (void);
+void right (void);
+void left (void);
+void detection_on (void);
+void detection_off (void);
 
 #endif /* UART_H_ */
 
